a9b63131e515c619c96a9a21a8d502f9892b1ab9
Lauri <hakulinen.lauri@gmail.com>
2021-11-17 13:10:11 +0000

    Material UI picker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState }  from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
-  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todo, setTodo] = useState({desc: '',date: new Date()});
   const [todos, setTodos] = useState([]);
 
   const addTodo = (event) => {
@@ -12,22 +16,36 @@ function App() {
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value});
-  } 
+  }
+
+  const dateChanged = (date) => {
+    const [year, month, day] = [date.getFullYear(), date.getMonth()+1, date.getDate()];
+    let dateString = year+'/'+month+'/'+day;
+    setTodo({...todo, date: dateString});
+  }
 
   return (
     <div className="App">
-      <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
-      <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
-      <button onClick={addTodo}>Add</button>
-      <table><tbody>
-      {
-      todos.map((todo, index) => 
-        <tr key={index}>
-          <td>{todo.date}</td>
-          <td>{todo.desc}</td>
-        </tr>)
-      }
-      </tbody></table>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DatePicker size="small" name="date" label="Date" value={todo.date} onChange={date => dateChanged(date)}/>
+      </MuiPickersUtilsProvider>
+      <TextField size="small" name="desc" label="Description" variant="standard" onChange={inputChanged} value={todo.desc}/>
+      <Button size="large" variant="contained" onClick={addTodo}>Add</Button>
+      <table>
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+        {
+        todos.map((todo, index) => 
+          <tr key={index}>
+            <td>{todo.date}</td>
+            <td>{todo.desc}</td>
+          </tr>)
+        }
+        </tbody>
+      </table>
     </div>
   );
 }
